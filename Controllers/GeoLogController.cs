b0d5168bf6a803e086c776f8a8337a539c322fd2
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2021-10-27 17:05:22 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/GeoLogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoLogBackend.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class GeoLogController : ControllerBase
+    {
+       
+    }
+}
