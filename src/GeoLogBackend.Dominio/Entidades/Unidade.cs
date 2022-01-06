7a70ba2c554fcbe244bed8a9c9aac95d5ac77293
Matheus Magalhaes <matheusibrahimfm@gmail.com>
2022-01-06 14:41:17 -0300

    Ajustes nas propriedades para leitura do objeto JSON

--- src/GeoLogBackend.Dominio/Entidades/Unidade.cs
+++ src/GeoLogBackend.Dominio/Entidades/Unidade.cs
@@ -1,12 +1,12 @@
 ﻿using GeoLogBackend.GeoLogBackend.Dominio.Interfaces;
-
-
+using Newtonsoft.Json;
 
 namespace GeoLogBackend.Dominio
 {
     public class Unidade : Entidade, IAggregateRoot
     {
         public string Nome { get; set; }
+        [JsonProperty("simbolo")]
         public string Simbolo { get; set; }
         public double Multiplicador { get; set; }
     }
