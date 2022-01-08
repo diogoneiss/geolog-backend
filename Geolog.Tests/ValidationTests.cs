b115a01aca2c63130474e6da06cddfcb15066950
Diogo Neiss <40122099+diogoneiss@users.noreply.github.com>
2022-01-08 12:15:25 -0300

    Validacao, dto e testes implementados

---
+++ Geolog.Tests/ValidationTests.cs
@@ -0,0 +1,42 @@
+﻿using GeoLogBackend.GeoLogBackend.Api.Validations;
+using GeoLogBackend.GeoLogBackend.Dominio.Entidades.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace GeoLogBackend.Api.Tests
+{
+   public class ValidationTests
+    {
+        private readonly GetPaisDtoValidation validation;
+
+        public ValidationTests()
+        {
+            validation = new GetPaisDtoValidation();
+        }
+        [Theory]
+        [Trait("Pais", "PaisGetRequest")]
+        [InlineData("", false)]
+        [InlineData("aa", false)]
+        [InlineData("andorra", true)]
+        [InlineData("andorra1", false)]
+        [InlineData("11121", false)]
+        [InlineData("21", false)]
+
+
+
+        public void DeveValidarNome(string nome, bool aprovar)
+
+        {
+            var request = new PaisGetDto(nome);
+
+            bool ehInstaciaValida = validation.Validate(request).IsValid;
+
+
+            Assert.Equal(aprovar, ehInstaciaValida);
+
+        }
+    }
+}
---
+++ src/GeoLogBackend.Api/Validations/GetPaisDtoValidation.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using GeoLogBackend.GeoLogBackend.Dominio.Entidades.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoLogBackend.GeoLogBackend.Api.Validations
+{
+    
+    
+        public class GetPaisDtoValidation : AbstractValidator<PaisGetDto>
+        {
+            public GetPaisDtoValidation()
+            {
+            //nao pode ser menor que 3 e nem conter numeros
+            RuleFor(p => p.Nome)
+            .NotNull()
+            .NotEmpty()
+            .MinimumLength(3)
+            .Must(p => !p.Any(char.IsDigit))
+            .WithMessage("Pais nao deve conter numeros");
+            }
+        }
+}
